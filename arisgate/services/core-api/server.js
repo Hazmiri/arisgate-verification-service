03d685ff40809fff840495c323724a7215a2b5fd
Saladin <hazmiri-sm@hotmail.com>
2026-03-11 08:22:51 +0000

    Fix server log message to display running

--- arisgate/services/core-api/server.js
+++ arisgate/services/core-api/server.js
@@ -1,10 +1,11 @@
 //Load environment variables from .env file
-require("dotenv").config();
+require("dotenv").config()
 //Import the Express application
-const app = require("./src/app");
+const app = require("./src/app")
 //Define the port the server will run on
-const Port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 //Start the server
-app.listen(Port, () => {
-  console.log("ArisGate API running on port ${PORT}");
-});
+app.listen(PORT, () => {
+  // correct template string using backticks
+  console.log(`ArisGate API running on port ${PORT}`)
+})
